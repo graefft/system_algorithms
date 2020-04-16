3e37f5ba068679da51d0a6fbb03c50d7ee8d334d
Thomas Graeff <676@holbertonschool.com>
2020-04-16 09:17:02 +0000

    Adds header file

---
+++ 0x00-red_black_tree/rb_trees.h
@@ -0,0 +1,45 @@
+#ifndef __RB_TREES_H__
+#define __RB_TREES_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * enum rb_color_e - Possible color of a Red-Black tree
+ *
+ * @RED: 0 -> Red node
+ * @BLACK: 1 -> Black node
+ * @DOUBLE_BLACK: Double Black node
+ */
+typedef enum rb_color_e
+{
+	RED = 0,
+	BLACK,
+	DOUBLE_BLACK
+} rb_color_t;
+
+/**
+ * struct rb_tree_s - Red-Black tree node structure
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ * @color: Color of the node (RED or BLACK)
+ */
+typedef struct rb_tree_s
+{
+	int n;
+	rb_color_t color;
+	struct rb_tree_s *parent;
+	struct rb_tree_s *left;
+	struct rb_tree_s *right;
+} rb_tree_t;
+
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color);
+int rb_tree_is_valid(const rb_tree_t *tree);
+rb_tree_t *rb_tree_insert(rb_tre_t **tree, int value);
+rb_tree_t *array_to_rb_tree(int *array, size_t size);
+rb_tree *rb_tree_remove(rb_tree_t *root, int n);
+
+#endif /*__RB_TREES_H__
