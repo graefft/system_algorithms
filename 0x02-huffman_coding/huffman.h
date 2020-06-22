18cda98f375a3ce198646a19d72ec16b12a9c00b
Thomas Graeff <676@holbertonschool.com>
2020-06-22 18:17:32 +0000

    Adds Huffman header

---
+++ 0x02-huffman_coding/huffman.h
@@ -0,0 +1,25 @@
+#ifndef __HUFFMAN_H__
+#define __HUFFMAN_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct symbol_s - Stores a char and its associated frequency
+ *
+ * @data: The character
+ * @freq: The associated frequency
+ */
+typedef struct symbol_s
+{
+	char data;
+	size_t freq;
+} symbol_t;
+
+symbol_t *symbol_create(char data, size_t freq);
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
+int huffman_extract_and_insert(heap_t *priority_queue);
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size);
+int huffman_codes(char *data, size_t *freq, size_t size);
+
+#endif /*__HUFFMAN_H__*/
