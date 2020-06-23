304c715d4e3eb76197ec44eda1acd6046ea5faac
Thomas Graeff <676@holbertonschool.com>
2020-06-23 06:41:24 +0000

    Initial add of function to create binary_tree_node_t

---
+++ 0x02-huffman_coding/heap/binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "heap.h"
+
+/**
+ * binary_tree_node - creates generic Binary Tree node
+ * @parent: pointer to parent node of node to be created
+ * @data: data to be stored in the node
+ * Return: pointer to created node, or NULL on failure
+ */
+binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data)
+{
+	binary_tree_node_t *new_node;
+
+	if (!data)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_node_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->data = data;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
