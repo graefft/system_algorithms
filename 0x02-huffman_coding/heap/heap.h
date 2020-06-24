d9fac44f8e30f7ea3b94d7e85231ac05f319f227
Thomas Graeff <676@holbertonschool.com>
2020-06-24 06:43:48 +0000

    Updates header

--- 0x02-huffman_coding/heap/heap.h
+++ 0x02-huffman_coding/heap/heap.h
@@ -36,7 +36,10 @@ typedef struct heap_s
 
 heap_t *heap_create(int (*data_cmp)(void *, void *));
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
+
 binary_tree_node_t *heap_insert(heap_t *heap, void *data);
+binary_tree_node_t *heapify(binary_tree_t *node);
+
 void *heap_extract(heap_t *heap);
 void heap_delete(heap_t *heap, void (*free_data)(void *));
 
