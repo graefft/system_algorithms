a22edbf0f1bab262779e8eb02f751422cafe61cf
Thomas Graeff <676@holbertonschool.com>
2020-08-26 22:47:03 -0700

    Initial commit of header file for nary_trees project

---
+++ 0x04-nary_trees/nary_trees.h
@@ -0,0 +1,28 @@
+#ifndef __NARY_TREES_H__
+#define __NARY_TREES_H__
+
+/**
+ * struct nary_tree_s - N-ary tree node structure
+ *
+ * @content: Content of the node
+ * @parent: Pointer to the parent node
+ * @nb_children: Number of children
+ * @children: Pointer to the head node of the children linked list
+ * @next: Pointer to the next child in the parent's children linked list
+ */
+typedef struct nary_tree_s
+{
+    char *content;
+	size_t nb_children
+    struct nary_tree_s  *parent;
+    struct nary_tree_s  *children;
+    struct nary_tree_s  *next;
+} nary_tree_t;
+
+nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str);
+void nary_tree_delete(nary_tree_t *tree);
+size_t nary_tree_traverse(nary_tree_t const *root, void (*action)(nary_tree_t const *node, size_t depth));
+size_t nary_tree_diameter(nary_tree_t const *root);
+int path_exists(nary_tree_t const *root, char const * const *path);
+
+#endif /* __NARY_TREES_H__ */
