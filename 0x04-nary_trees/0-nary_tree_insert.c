306a2d06ed5e74e91a5cd3e1f3e7b5b4778de75e
Thomas Graeff <676@holbertonschool.com>
2020-08-30 16:21:11 -0700

    Set ->content to strdup of str

--- 0x04-nary_trees/0-nary_tree_insert.c
+++ 0x04-nary_trees/0-nary_tree_insert.c
@@ -10,14 +10,12 @@
 nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
 {
 	nary_tree_t *new_tree_node;
-	char *string_dup;
 
 	new_tree_node = malloc(sizeof(nary_tree_t));
 	if (!new_tree_node)
 		return (NULL);
 
-	string_dup = strdup((char *)str);
-	new_tree_node->content = string_dup;
+	new_tree_node->content = strdup(str);
 	new_tree_node->nb_children = 0;
 	new_tree_node->children = NULL;
 	if (parent)
@@ -32,6 +30,5 @@ nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
 		new_tree_node->parent = NULL;
 		new_tree_node->next = NULL;
 	}
-	free(string_dup);
 	return (new_tree_node);
 }
