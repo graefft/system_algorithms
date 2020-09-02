9285d8b968d35bd51a4dc4ff2ee6d4a93a1934e8
Thomas Graeff <676@holbertonschool.com>
2020-09-02 00:07:13 -0700

    Finished

--- 0x04-nary_trees/3-nary_tree_diameter.c
+++ 0x04-nary_trees/3-nary_tree_diameter.c
@@ -7,7 +7,27 @@
  * @diameter: maximum diameter between two nodes
  * Return: max height
  */
-void max_height
+size_t max_height(nary_tree_t const *root, int *diameter)
+{
+	nary_tree_t *child;
+	size_t max_1 = 0;
+	size_t max_2 = 0;
+	size_t height;
+
+	for (child = root->children; child; child = child->next)
+	{
+		height = max_height(child);
+		if (height >= max_1)
+		{
+			max_2 = max_1;
+			max_1 = height;
+		}
+		else if (height > max_2)
+			max_2 = height;
+	}
+	*diameter = MAX(diameter, max_1 + max_2);
+	return (MAX(max_1, max_2));
+}
 
 /**
  * nary_tree_diameter - computes the diameter of N-ary tree
